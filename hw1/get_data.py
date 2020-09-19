f59441b354ee61c9c30c05f39cdb201b6da6cce4
Amrit Singhal <amritsinghal97@gmail.com>
2020-09-18 20:06:21 -0400

    Fix table deletion

--- hw1/get_data.py
+++ hw1/get_data.py
@@ -44,6 +44,7 @@ if args.store:
     drop_command = f"DROP TABLE IF EXISTS {schema_name}.{table_name};"
     print(drop_command)
     conn.commit()
+    sleep(2)
 
     # creating table
     create_command = os.popen(f"cat data.csv | tr [:upper:] [:lower:] | tr ' ' '_' | sed 's/#/num/' | " + \
